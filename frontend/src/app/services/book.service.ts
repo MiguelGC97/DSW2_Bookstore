67c6809609aa336e9eae68fd0a29d16854463223
MiguelGC97 <miguelangelgutierrezcarreno@alumno.ieselrincon.es>
2024-10-02 19:38:28 +0100

    getBooks added to service

--- frontend/src/app/services/book.service.ts
+++ frontend/src/app/services/book.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  endpoint = `http://localhost:8080/api/my-books`
+
+  constructor(private httpClient: HttpClient) { }
+
+  getBooks() {
+    return this.httpClient.get(this.endpoint);
+  }
 }
