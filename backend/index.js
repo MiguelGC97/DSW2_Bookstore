4e9bd770df235126ddb32fba5f8130890e995635
MiguelGC97 <miguelangelgutierrezcarreno@alumno.ieselrincon.es>
2024-10-02 09:59:46 +0100

    cors installed and added to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8100"
+};
 
 app.use(cors(corsOptions));
 
